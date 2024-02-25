e13bd27117abc27db3361ec82f94334761f74c55
topsy <Malatjimj2@gmail.com>
2024-02-26 00:00:00 +0200

    Integrated gateway

--- app/utils/db.ts
+++ app/utils/db.ts
@@ -6,3 +6,4 @@ const globalForPrisma = global as unknown as { prisma: PrismaClient };
 export const prisma = globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+// Modified on 2025-02-19 00:38:38
