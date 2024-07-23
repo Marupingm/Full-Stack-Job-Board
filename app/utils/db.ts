25f305cee23722fd57e5838be9e9c90ae1ade219
topsy <Malatjimj2@gmail.com>
2024-07-24 00:00:00 +0200

    Refactored code

--- app/utils/db.ts
+++ app/utils/db.ts
@@ -7,3 +7,4 @@ export const prisma = globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 // Modified on 2025-02-19 00:38:38
+// Modified on 2025-02-19 00:56:04
